9dd070d78369008d3aa1781cba8d0fbe1c6b69d6
Dr. Janet Duffy REALTOR License# S.0197614.LLC <DrDuffy@bhhsnv.com>
2025-05-28 06:03:46 -0700

    Fix sitemap 404 error - add server routes for SEO

--- server/routes.ts
+++ server/routes.ts
@@ -5,6 +5,47 @@ import { insertLeadSchema, insertPropertySchema, insertMarketDataSchema } from "
 import { crmIntegration } from "./crm-integration";
 
 export async function registerRoutes(app: Express): Promise<Server> {
+export async function registerRoutes(app: Express): Promise<Server> {):
+
+  // SEO Routes
+  app.get("/sitemap.xml", (req, res) => {
+    res.set('Content-Type', 'text/xml');
+    res.send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://shawoodhomes.com/</loc>
+    <lastmod>2025-01-28</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://shawoodhomes.com/homes</loc>
+    <lastmod>2025-01-28</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://shawoodhomes.com/about</loc>
+    <lastmod>2025-01-28</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://shawoodhomes.com/home-value</loc>
+    <lastmod>2025-01-28</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+</urlset>`);
+  });
+  app.get("/robots.txt", (req, res) => {
+    res.set('Content-Type', 'text/plain');
+    res.send(`User-agent: *
+Allow: /
+Sitemap: https://shawoodhomes.com/sitemap.xml
+Host: shawoodhomes.com`);
+  });
+  
   // Properties
   app.get("/api/properties", async (req, res) => {
     try {
